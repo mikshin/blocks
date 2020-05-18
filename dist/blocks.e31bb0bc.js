924c2af96dda6f97f61e8996ac32c3354eeec2b8
mikshin <dimamikshin@gmail.com>
2020-05-19 00:04:16 +0300

    Сделал определение ближайшего (одного)

--- dist/blocks.e31bb0bc.js
+++ dist/blocks.e31bb0bc.js
@@ -119,8 +119,10 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   return newRequire;
 })({"index.js":[function(require,module,exports) {
 var blocks = document.querySelectorAll('.blocks-item');
-var view = document.querySelector('.view');
-window.onload = init;
+var blocksContainer = document.querySelector('.blocks');
+var viewContainer = document.querySelector('.view');
+var coef = 25;
+window.onload = init; // инициализация
 
 function init() {
   for (var i = 0; blocks.length > i; i++) {
@@ -134,29 +136,83 @@ function init() {
 }
 
 function moving(block) {
-  block.classList.add('blocks-item--moving'); // moveTo(block);
+  block.style.position = "fixed";
+  block.style.zIndex = "1";
+  eventsNone("none"); // событие начала
 
   document.onmousemove = function (e) {
     moveTo(block);
-  };
+  }; // событие окончания
 
-  block.onmouseup = function () {
-    block.classList.remove('blocks-item--moving');
+
+  document.onmouseup = function () {
+    eventsNone("");
     inject(block);
+    building(block);
     document.onmousemove = null;
     block.onmouseup = null;
   };
-}
+} // перемещение за курсором
+
 
 function moveTo(block) {
   block.style.left = event.pageX - block.offsetWidth / 2 + 'px';
   block.style.top = event.pageY - block.offsetHeight / 2 + 'px';
-}
+} // перемещаем в нужный блок
+
 
 function inject(block) {
-  var tmp = block;
-  block.remove();
-  console.log(event.target);
+  checkPosition().append(block);
+
+  if (checkPosition() == blocksContainer) {
+    block.style.position = "";
+    block.style.top = "";
+    block.style.left = "";
+    block.style.zIndex = "";
+  }
+} // проверяем в какой блок попадает
+
+
+function checkPosition() {
+  if (event.target == blocksContainer) {
+    return blocksContainer;
+  }
+
+  if (event.target == viewContainer) {
+    return viewContainer;
+  }
+} // функция обнуления всех ивентов
+
+
+function eventsNone(tmp) {
+  for (var i = 0; blocks.length > i; i++) {
+    blocks[i].style.pointerEvents = tmp;
+  }
+}
+
+function building(block) {
+  var closest = findClosest(block);
+}
+
+function findClosest(block) {
+  if (block.parentNode == viewContainer) {
+    var neighbors = block.parentNode.querySelectorAll(".blocks-item"),
+        x = block.getBoundingClientRect().left,
+        y = block.getBoundingClientRect().top;
+
+    for (var i = 0; i < neighbors.length; i++) {
+      if (neighbors[i] != block) {
+        var neighborY = neighbors[i].getBoundingClientRect().top,
+            neighborX = neighbors[i].getBoundingClientRect().left,
+            neighborWidth = neighborX + neighbors[i].offsetWidth,
+            neighborHeight = neighborY + neighbors[i].offsetHeight;
+
+        if (y + block.offsetHeight > neighborY - coef && neighborHeight + coef > y && x + block.offsetWidth > neighborX - coef && neighborWidth + coef > x) {
+          return neighbors[i];
+        }
+      }
+    }
+  }
 }
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
--- index.js
+++ index.js
@@ -1,42 +1,92 @@
 var blocks = document.querySelectorAll('.blocks-item');
-var view = document.querySelector('.view');
-
+var blocksContainer = document.querySelector('.blocks');
+var viewContainer = document.querySelector('.view');
+var coef = 25;
 
 window.onload = init;
-
+// инициализация
 function init() {
-    for(var i = 0; blocks.length > i; i++) {
+    for (var i = 0; blocks.length > i; i++) {
         blocks[i].style.backgroundColor = blocks[i].innerHTML;
         blocks[i].id = "block_" + i;
-        blocks[i].onmousedown = function(e) {
+        blocks[i].onmousedown = function (e) {
             moving(this)
         }
     }
 }
 
 function moving(block) {
-    block.classList.add('blocks-item--moving')
-    // moveTo(block);
-
-    document.onmousemove = function(e) {
+    block.style.position = "fixed";
+    block.style.zIndex = "1";
+    eventsNone("none");
+    // событие начала
+    document.onmousemove = function (e) {
         moveTo(block);
     }
-
-    block.onmouseup = function () {
-        block.classList.remove('blocks-item--moving')
-        inject(block)
+    // событие окончания
+    document.onmouseup = function () {
+        eventsNone("");
+        inject(block);
+        building(block);
         document.onmousemove = null;
         block.onmouseup = null;
     }
 }
-
+// перемещение за курсором
 function moveTo(block) {
     block.style.left = event.pageX - block.offsetWidth / 2 + 'px';
     block.style.top = event.pageY - block.offsetHeight / 2 + 'px';
 }
-
+// перемещаем в нужный блок
 function inject(block) {
-    var tmp = block;
-    block.remove();
-    console.log(event.target)
+    checkPosition().append(block)
+    if (checkPosition() == blocksContainer) {
+        block.style.position = "";
+        block.style.top = "";
+        block.style.left = "";
+        block.style.zIndex = "";
+    }
+}
+// проверяем в какой блок попадает
+function checkPosition() {
+    if (event.target == blocksContainer) {
+        return blocksContainer;
+    }
+    if (event.target == viewContainer) {
+        return viewContainer;
+    }
+}
+// функция обнуления всех ивентов
+function eventsNone(tmp) {
+    for (var i = 0; blocks.length > i; i++) {
+        blocks[i].style.pointerEvents = tmp;
+    }
+}
+
+function building(block) {
+    var closest = findClosest(block);
+
+}
+
+function findClosest(block) {
+    if (block.parentNode == viewContainer) {
+        var neighbors = block.parentNode.querySelectorAll(".blocks-item"),
+            x = block.getBoundingClientRect().left,
+            y = block.getBoundingClientRect().top;
+
+        for (var i = 0; i < neighbors.length; i++) {
+            if (neighbors[i] != block) {
+
+                var neighborY = neighbors[i].getBoundingClientRect().top,
+                    neighborX = neighbors[i].getBoundingClientRect().left,
+                    neighborWidth = neighborX + neighbors[i].offsetWidth,
+                    neighborHeight = neighborY + neighbors[i].offsetHeight;
+
+                if (y + block.offsetHeight > neighborY - coef && neighborHeight + coef > y && x + block.offsetWidth > neighborX - coef && neighborWidth + coef > x) {
+                    return (neighbors[i])
+                }
+            }
+        }
+
+    }
 }
